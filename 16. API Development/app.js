f1f4d4fbf3e260611284ec09aa4e955246d68b09
abdulhaseeb0666 <abdulhaseebah7886786@gmail.com>
2026-04-27 05:56:23 +0500

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   16. API Development/app.js
    	modified:   16. API Development/package-lock.json
    	modified:   16. API Development/package.json
    	new file:   17. Fetch API/index.html

--- 16. API Development/app.js
+++ 16. API Development/app.js
@@ -1,17 +1,24 @@
 import express from "express";
 const app = express();
+import cors from "cors";
+import path from "path";
+
+import userRoutes from "./routes/user.routes.js";
+import connectDB from "./config/database.js";
+
 
 // Database connection
-import connectDB from "./config/database.js";
 connectDB();
 
 // Middleware
 app.use(express.json());
 app.set("view engine", "ejs");
 app.use(express.urlencoded({extended : true}));
+app.use(cors());
+app.use("/uploads" ,express.static(path.join(process.cwd(), "uploads")));
+
 
 // Routes
-import userRoutes from "./routes/user.routes.js";
 app.use("/api/users", userRoutes);
 
 
